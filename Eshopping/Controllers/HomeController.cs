cbc77f632035731d913741bdfcc8512d98ee0d5f
tranhaidangdz <141541516+tranhaidangdz@users.noreply.github.com>
2024-09-23 11:22:27 +0700

    hien thi slider

--- Eshopping/Controllers/HomeController.cs
+++ Eshopping/Controllers/HomeController.cs
@@ -20,6 +20,9 @@ namespace Eshopping.Controllers
         public IActionResult Index()
         {
             var products =_dataContext.Products.Include("Category").Include("Brand").ToList();
+            var sliders= _dataContext.Sliders.Where(s=>s.Status==1).ToList();
+            ViewBag.Sliders=sliders;
+
             return View(products);
         }
 
