b0af05d961cddac291cc9daf5d6fa6a994490064
tranhaidangdz <141541516+tranhaidangdz@users.noreply.github.com>
2024-10-06 20:20:51 +0700

    api tinh thanh pho VN

---
+++ Eshopping/Areas/Admin/Controllers/ShippingController.cs
@@ -0,0 +1,21 @@
+﻿using Eshopping.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eshopping.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Route("Admin/Shipping")]
+	public class ShippingController : Controller
+	{
+        private readonly DataContext _dataContext;
+        public ShippingController(DataContext context)
+        {
+            _dataContext = context;
+        }
+		[Route("Index")]
+        public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
--- Eshopping/Repository/DataContext.cs
+++ Eshopping/Repository/DataContext.cs
@@ -21,5 +21,6 @@ namespace Eshopping.Repository
 		public DbSet<WishlistModel> Wishlists { get; set; }
 		public DbSet<CompareModel> Compares { get; set; }
 		public DbSet<ProductQuantityModel> ProductQuantities { get; set; }
+		public DbSet<ShippingModel> Shippings { get; set; }
 	}
 }
